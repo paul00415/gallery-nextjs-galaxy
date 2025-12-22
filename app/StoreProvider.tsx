ae9714acadfb1fe95f6eb9461f50611c5a4e0f5e
Endeavour <thepaul.work@gmail.com>
2025-12-22 22:38:30 +0900

    Feat: Implement Husky, prettier and lint

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useRef } from 'react';
+import { useState } from 'react';
 import { Provider } from 'react-redux';
 import type { AppStore } from '../lib/store';
 import { makeStore } from '../lib/store';
@@ -9,11 +9,7 @@ export default function StoreProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore | null>(null);
-  if (!storeRef.current) {
-    // Create the store instance the first time this renders
-    storeRef.current = makeStore();
-  }
+  const [store] = useState<AppStore>(() => makeStore());
 
-  return <Provider store={storeRef.current}>{children}</Provider>;
+  return <Provider store={store}>{children}</Provider>;
 }
